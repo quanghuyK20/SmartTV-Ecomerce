8a7adc818ff9d34b0972808917cbb427978e2ab9
quanghuyK20 <lqhuy.cl1@gmail.com>
2024-03-26 19:50:01 +0700

    Init src

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,29 @@
+<footer class="bg-dark">
+    <div class="container bg-dark">
+        <div class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="footer-link">Privacy policy</a><br>
+                    <a href="#" class="footer-link">Term and conditions</a><br>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Welcome to the HHTV
+                    </h3>
+                    <p style>Welcome to our website specialized in offering a wide range of high-quality TVs and providing attentive customer service, ensuring you experience excellent entertainment at home!</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Contact:
+                    </h3>
+                    <p>Email: huylq@gmail.com</p>
+                    <p>Website: <a href="">Personal Website</a></p>
+                    <p>Github: <a href="">bantivi</a></p>
+                    <p>Twitter: <a href="">bantivi</a></p>
+                    <p>LinkedIn: <a href="">bantivi</a></p>
+                </div>
+            </div>
+        </div>
+        <p>Huy Huong Tuan Vy 2024</p>
+    </div>
+</footer>
